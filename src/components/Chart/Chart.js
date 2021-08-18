5b27b152856eeb6ce40457cedd3154fd0bf3e6a5
maryamalrubaye <maryamalrubaye18@gmail.com>
2021-08-18 12:22:18 +0300

    adding chart

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -2,7 +2,7 @@ import React from "react";
 import ChartBar from "./ChartBar";
 import "./Chart.css";
 const Chart = (props) => {
-  const dataPointsValues = props.dataPoints.map((dataPoint) => dataPoint.value);
+  const dataPointsValues = props.dataPoint.map((dataPoint) => dataPoint.value);
   const totalMaximum = Math.max(...dataPointsValues);
   return (
     <div className="chart">
