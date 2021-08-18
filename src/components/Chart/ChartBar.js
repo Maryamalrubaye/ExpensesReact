61c951c2e45fa0b2eac4d262c6a61b2317371060
maryamalrubaye <maryamalrubaye18@gmail.com>
2021-08-18 11:56:59 +0300

    new changes

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,17 +1,23 @@
 import React from "react";
 import "./ChartBar.css";
+
 const ChartBar = (props) => {
-  let barFillHight = "0%";
-  if (props.max > 0) {
-    barFillHight = Math.round((props.max / props.maxValue) * 100) + "%";
+  let barFillHeight = "0%";
+  if (props.maxValue > 0) {
+    barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
   }
+
   return (
-    <div className="chart-bar">
+    <div className="Chart-bar">
       <div className="chart-bar__inner">
-        <div className="chart-bar__fill" style={{ height: barFillHight }}></div>
+        <div
+          className="chart-bar__fill"
+          style={{ height: barFillHeight }}
+        ></div>
       </div>
       <div className="chart-bar__label">{props.label}</div>
     </div>
   );
 };
+
 export default ChartBar;
