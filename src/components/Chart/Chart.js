6fabeeff5309150e4de23edc44544da42f34747c
maryamalrubaye <maryamalrubaye18@gmail.com>
2021-08-18 11:55:15 +0300

    new features

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -2,18 +2,19 @@ import React from "react";
 import ChartBar from "./ChartBar";
 import "./Chart.css";
 const Chart = (props) => {
-  const dataPointsValues = props.dataPoints.map(dataPoint =>dataPoint.value);
-  const totalMaximum =Math.max(...dataPointsValues);
+  const dataPointsValues = props.dataPoints.map((dataPoint) => dataPoint.value);
+  const totalMaximum = Math.max(...dataPointsValues);
   return (
-  <div className="chart">
-    {props.dataPoints.map((dataPoint) => (
-      <ChartBar
-        key={dataPoint.lable}
-        value={dataPoint.value}
-        maxValue={totalMaximum }
-        lable={dataPoint.lable}
-      />
-    ))}
-  </div>
-  )};
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          key={dataPoint.lable}
+          value={dataPoint.value}
+          maxValue={totalMaximum}
+          lable={dataPoint.lable}
+        />
+      ))}
+    </div>
+  );
+};
 export default Chart;
